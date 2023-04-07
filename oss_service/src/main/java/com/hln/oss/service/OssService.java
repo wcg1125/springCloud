f9e6cc824e8071a4667b488eb4c4596029bee089
wcg1125 <130132469+wcg1125@users.noreply.github.com>
2023-04-07 15:41:48 +0800

    Delete oss_service directory

--- oss_service/src/main/java/com/hln/oss/service/OssService.java
+++
@@ -1,8 +0,0 @@
-package com.hln.oss.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface OssService {
-
-    String uploadActFile(MultipartFile file,String source);
-}
